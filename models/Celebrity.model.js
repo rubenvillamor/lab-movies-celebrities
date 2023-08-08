fa49691225b830265b6630e2cdf383cfd75aa2b0
Ruben <villamorlage@gmail.com>
2023-08-08 15:46:19 +0200

    avance

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,24 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+// Esquema
+const celebritySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  catchPhrase: {
+    type: String,
+    required: true,
+  },
+});
+
+// ir a la base de datos
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+const celebrityRouter = require("./celebrities.routes.js")
+router.use("/celebrity", celebrityRouter)
+
+const movieRouter = require("./movies.routes.js")
+router.use("/movie", movieRouter)
+
 module.exports = router;
